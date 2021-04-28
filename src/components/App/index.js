385755c89ca5675e3e78532680379154f0acb382
rohit phatarpekar <rohitphatarpekar@scitechcentre.com>
2021-04-28 17:10:11 +0530

    functionality done

---
+++ src/components/App/index.js
@@ -0,0 +1,215 @@
+import React, { useEffect, useState } from "react";
+// import menu from "../../assets/menu.png";
+import Button from "../Button";
+import commafy from "../utils/commafy";
+import "./App.css";
+
+const App = () => {
+  const [time, setTime] = useState(new Date());
+  const [value, setValue] = useState("0");
+  const [memory, setMemory] = useState(null);
+  const [operator, setOperator] = useState(null);
+
+  // useEffect(() => {
+  //   setTime(new Date());
+  // }, [new Date().getMinutes()]);
+
+  const handleButtonPress = (content) => () => {
+    const num = parseFloat(value);
+
+    if (content === "AC") {
+      setValue("0");
+      setMemory(null);
+      setOperator(null);
+      return;
+    }
+
+    if (content === "±") {
+      setValue((num * -1).toString());
+      return;
+    }
+
+    if (content === "%") {
+      setValue((num / 100).toString());
+      setMemory(null);
+      setOperator(null);
+      return;
+    }
+
+    if (content === ".") {
+      if (value.includes(".")) return;
+
+      setValue(value + ".");
+      return;
+    }
+
+    if (content === "+") {
+      if (operator !== null) {
+        if (operator === "+") {
+          setMemory(memory + parseFloat(value));
+        } else if (operator === "−") {
+          setMemory(memory - parseFloat(value));
+        } else if (operator === "×") {
+          setMemory(memory * parseFloat(value));
+        } else if (operator === "÷") {
+          setMemory(memory / parseFloat(value));
+        }
+      } else {
+        setMemory(parseFloat(value));
+      }
+      setValue("0");
+      setOperator("+");
+      return;
+    }
+    if (content === "−") {
+      if (operator !== null) {
+        if (operator === "+") {
+          setMemory(memory + parseFloat(value));
+        } else if (operator === "−") {
+          setMemory(memory - parseFloat(value));
+        } else if (operator === "×") {
+          setMemory(memory * parseFloat(value));
+        } else if (operator === "÷") {
+          setMemory(memory / parseFloat(value));
+        }
+      } else {
+        setMemory(parseFloat(value));
+      }
+      setValue("0");
+      setOperator("−");
+      return;
+    }
+    if (content === "×") {
+      if (operator !== null) {
+        if (operator === "+") {
+          setMemory(memory + parseFloat(value));
+        } else if (operator === "−") {
+          setMemory(memory - parseFloat(value));
+        } else if (operator === "×") {
+          setMemory(memory * parseFloat(value));
+        } else if (operator === "÷") {
+          setMemory(memory / parseFloat(value));
+        }
+      } else {
+        setMemory(parseFloat(value));
+      }
+      setValue("0");
+      setOperator("×");
+      return;
+    }
+    if (content === "÷") {
+      if (operator !== null) {
+        if (operator === "+") {
+          setMemory(memory + parseFloat(value));
+        } else if (operator === "−") {
+          setMemory(memory - parseFloat(value));
+        } else if (operator === "×") {
+          setMemory(memory * parseFloat(value));
+        } else if (operator === "÷") {
+          setMemory(memory / parseFloat(value));
+        }
+      } else {
+        setMemory(parseFloat(value));
+      }
+      setValue("0");
+      setOperator("÷");
+      return;
+    }
+    if (content === "x²") {
+      setValue((num * num).toString());
+      setMemory(null);
+      setOperator(null);
+      return;
+    }
+    if (content === "SQ") {
+      setValue(Math.sqrt(num).toString());
+      setMemory(null);
+      setOperator(null);
+      return;
+    }
+    if (content === "1/x") {
+      setValue((1 / num).toString());
+      setMemory(null);
+      setOperator(null);
+      return;
+    }
+
+    if (content === "=") {
+      if (!operator) return;
+
+      if (operator === "+") {
+        setValue((memory + parseFloat(value)).toString());
+      } else if (operator === "−") {
+        setValue((memory - parseFloat(value)).toString());
+      } else if (operator === "×") {
+        setValue((memory * parseFloat(value)).toString());
+      } else if (operator === "÷") {
+        setValue((memory / parseFloat(value)).toString());
+      } else if (operator === "x²") {
+        setValue((memory / parseFloat(value)).toString());
+      } else if (operator === "SQ") {
+        setValue((memory / parseFloat(value)).toString());
+      } else if (operator === "1/x") {
+        setValue((memory / parseFloat(value)).toString());
+      }
+
+      setMemory(null);
+      setOperator(null);
+      return;
+    }
+
+    if (value[value.length - 1] === ".") {
+      setValue(value + content);
+    } else {
+      setValue(parseFloat(num + content).toString());
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="display">{commafy(value)}</div>
+      <div className="buttons">
+        <Button
+          onButtonClick={handleButtonPress}
+          content="AC"
+          type="function"
+        />
+        <Button onButtonClick={handleButtonPress} content="±" type="function" />
+        <Button onButtonClick={handleButtonPress} content="%" type="function" />
+        <Button onButtonClick={handleButtonPress} content="÷" type="operator" />
+        <Button onButtonClick={handleButtonPress} content="7" />
+        <Button onButtonClick={handleButtonPress} content="8" />
+        <Button onButtonClick={handleButtonPress} content="9" />
+        <Button onButtonClick={handleButtonPress} content="×" type="operator" />
+        <Button onButtonClick={handleButtonPress} content="4" />
+        <Button onButtonClick={handleButtonPress} content="5" />
+        <Button onButtonClick={handleButtonPress} content="6" />
+        <Button onButtonClick={handleButtonPress} content="−" type="operator" />
+        <Button onButtonClick={handleButtonPress} content="1" />
+        <Button onButtonClick={handleButtonPress} content="2" />
+        <Button onButtonClick={handleButtonPress} content="3" />
+        <Button onButtonClick={handleButtonPress} content="+" type="operator" />
+        <Button onButtonClick={handleButtonPress} content="0" />
+        <Button onButtonClick={handleButtonPress} content="." />
+        <Button onButtonClick={handleButtonPress} content="=" type="operator" />
+        <Button
+          onButtonClick={handleButtonPress}
+          content="x²"
+          type="operator"
+        />
+        <Button
+          onButtonClick={handleButtonPress}
+          content="SQ"
+          type="operator"
+        />
+        <Button
+          onButtonClick={handleButtonPress}
+          content="1/x"
+          type="operator"
+        />
+      </div>
+    </div>
+  );
+};
+
+export default App;
